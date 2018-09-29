11311e1bd58ac566870b66caa61b0eeaed0a0cc8
bingoo <bingoo.huang@gmail.com>
2018-09-29 10:30:54 +0800

    add test case for emoji and dbc quotes

--- src/test/java/com/github/bingoohuang/UtilTest.java
+++ src/test/java/com/github/bingoohuang/UtilTest.java
@@ -1,5 +1,6 @@
 package com.github.bingoohuang;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -12,5 +13,16 @@ public class UtilTest {
         assertThat(Util.displayLength("权限,true开启，fasle关闭")).isEqualTo(24);
         assertThat(Util.displayLength("┌")).isEqualTo(1);
         assertThat(Util.displayLength(" ┌─────┬─────┐ ")).isEqualTo(15);
+
+    }
+
+    @Test @Ignore
+    public void emoji() {
+        assertThat(Util.displayLength("会籍为：小✈️")).isEqualTo(12);
+    }
+
+    @Test @Ignore
+    public void quote() {
+        assertThat(Util.displayLength("用于活动“好评有礼”")).isEqualTo(18);
     }
 }
